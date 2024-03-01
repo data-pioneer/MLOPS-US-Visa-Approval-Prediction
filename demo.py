55d1302d72cb599710705b4a7c76ba02ac307049
Jaspreet Kaur <jasika.lpu@gmail.com>
2024-03-01 14:35:17 +0530

    MongoDb, data ingestion code added

--- demo.py
+++ demo.py
@@ -1,10 +1,19 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
 
-try:
-    a = 1 / "10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
+# try:
+#     a = 1 / "10"
+# except Exception as e:
+#     logging.info(e)
+#     raise USvisaException(e, sys) from e
 
+
+
+# second demo 
+
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+
+pipline  = TrainPipeline()
+pipline.run_pipeline()
